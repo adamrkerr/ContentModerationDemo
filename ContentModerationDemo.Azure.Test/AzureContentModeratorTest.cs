2ba760de18eb98c508addf96657e794e7cf49f2e
Adam Kerr <adamrkerr@gmail.com>
2017-12-21 23:26:49 -0600

    azure is hideous but functional

---
+++ ContentModerationDemo.Azure.Test/AzureContentModeratorTest.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContentModerationDemo.Azure.Test
+{
+    public class AzureContentModeratorTest
+    {
+        [Fact]
+        public async Task TestImageStream()
+        {
+            var moderator = new AzureContentModerator();
+
+            //Set a path to an image that will get results
+            var imagePath = @"C:\Users\Adam\Pictures\content moderation\zlonp3btp0d37l4ls3dy.jpg";
+
+            byte[] byteArray;
+            using (var stream = new FileStream(imagePath, FileMode.Open))
+            {
+                var byteList = new List<byte>();
+                for (int i = 0; i < stream.Length; i++)
+                {
+                    byteList.Add((byte)stream.ReadByte());
+                }
+
+                byteArray = byteList.ToArray();
+            }
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.keys.json", optional: false); //TODO: make your own keys file
+
+            var config = builder.Build();            
+
+            moderator.ApiKey = config.GetSection("Key").Value;
+
+            var result = await moderator.AnalyzeImage(byteArray);
+
+            Assert.False(result.Pass);
+            Assert.NotEmpty(result.ModerationScores);
+        }
+    }
+}
--- ContentModerationDemo.Azure/AzureContentModerator.cs
+++ ContentModerationDemo.Azure/AzureContentModerator.cs
@@ -1,13 +1,48 @@
 ﻿using ContentModerationDemo.Abstraction;
+using RestSharp;
 using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace ContentModerationDemo.Azure
 {
     public class AzureContentModerator : IContentModerator
     {
-        public Task<ModerationResponse> AnalyzeImage(byte[] imageBytes)
+        public string ApiKey { get; set; }
+        public async Task<ModerationResponse> AnalyzeImage(byte[] imageBytes)
         {
+            //var client = new RestClient("https://eastus2.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0");
+
+            //var request = new RestRequest("ProcessImage/Evaluate", Method.POST);
+            //request.AddFile("value", filePath);
+            //request.AddHeader("Content-Type", "image/jpeg");
+            //request.AddHeader("Ocp-Apim-Subscription-Key", ApiKey);
+
+            //var response = client.Execute(request);
+
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ApiKey);
+
+            // Request parameters
+            queryString["CacheImage"] = "false";
+            var uri = "https://eastus2.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessImage/Evaluate?" + queryString;
+
+            HttpResponseMessage response;
+            
+            using (var content = new ByteArrayContent(imageBytes))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+                response = await client.PostAsync(uri, content);
+
+                var responseValue = await response.Content.ReadAsStringAsync();
+            }
+
             throw new NotImplementedException();
         }
     }
