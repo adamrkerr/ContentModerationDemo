e9a6b911676bdeb7ae77137f235e85398ce249e6
Adam Kerr <adamrkerr@gmail.com>
2017-12-21 19:50:22 -0600

    begin aws integration

---
+++ ContentModerationDemo.AWS/AWSContentModerator.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ContentModerationDemo.AWS
+{
+    public class AWSContentModerator
+    {
+        public async Task<object> AnalyzeImage(byte[] imageBytes)
+        {
+            var endpoint = Amazon.RegionEndpoint.USWest2;
+            using (var client  = new AmazonRekognitionClient(endpoint))
+            {
+                var request = new DetectModerationLabelsRequest()
+                {
+                    Image = new Image()
+                    {
+                        Bytes = new MemoryStream(imageBytes)
+                    }
+                };
+
+                var responde = await client.DetectModerationLabelsAsync(request);
+
+                return responde;
+            }
+        }
+    }
+}
